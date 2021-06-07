333c4907fecc310a0aae30d36532f76ab7eb373d
Vladislav Teplov <61516555+shadowsmain@users.noreply.github.com>
2021-06-07 11:39:09 +0500

    Add files via upload

--- student.py
+++ student.py
@@ -4,11 +4,29 @@
 #for i in books:
 #    print(i)
 
-class Student:
+#class Students:
 
-    name = "Vladislav"
-    birthday = "20.01.2002"
-    adress = "bluxera 10"
+#def __init__(self, name, birthdate, adress):
+#    self.name = name
+ #   self.birthdate = "20.01.2002"
+   # self.adress = "bluxera 10"
 
-    def start(self):
-        print("Студент 33 группы")
+#    def start(self):
+#        print("Студент 33 группы")
+
+class Students:
+
+    def __init__(self, name, date_brithday, address):
+        self.name = name
+        self.date_brithday = date_brithday
+        self.address = address
+
+    def display_info(self):
+        print('Студентs: ', self.name)
+
+
+class group:
+
+    def __init__(self, name):
+        self.name = name
+        self.student = Student
